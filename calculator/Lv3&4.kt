30c233b5e59cbea0c68d18ee2f9d8db254b90226
Go9ma9wo <qlxlbp@gmail.com>
2023-12-05 15:35:15 +0900

    Calculator Homework

---
+++ calculator/Lv3&4.kt
@@ -0,0 +1,11 @@
+package com.example.calculator
+
+fun main() {
+
+    val num1 = 1
+    val num2 = 2
+
+    val calc = Calculator(AddOperation())
+    println("${num1} 더하기 ${num2} 결과는 : ${calc.operate(1, 2)} 입니다")
+
+}
